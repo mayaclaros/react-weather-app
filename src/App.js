72aa7d9e848f9bb5ad80ad90dd4128c5b3cdcffa
mayaclaros <mayawclaros@gmail.com>
2024-12-08 23:35:41 -0500

    App.js
    added proper spaces to links and updated links

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://www.justmeomaya.com/" target="_blank" rel="noreferrer">
-          Maya Claros
-        </a>{" "}
-        and is open sourced on{" "}
-        <a
-          href="
+      <div className="container">
+        <h1>Weather App</h1>
+        <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.justmeomaya.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Maya Claros
+          </a>{" "}
+          and is open sourced on{" "}
+          <a
+            href="
           https://github.com/mayaclaros/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Github
-        </a>{" "}
-        and hosted on
-        <a
-          href="https://react-weather-app-maya.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          Netlify
-        </a>
-      </footer>
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>{" "}
+          and hosted on
+          <a
+            href="https://react-weather-app-maya.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
