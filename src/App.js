d9625ef3a0cf082d8304d0ed4845a9a6d8db4140
mayaclaros <mayawclaros@gmail.com>
2024-12-08 22:40:54 -0500

    Update App.js
    added git url, netlify url, and persoanl website

--- src/App.js
+++ src/App.js
@@ -4,6 +4,30 @@ function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://www.justmeomaya.com/" target="_blank" rel="noreferrer">
+          Maya Claros
+        </a>{" "}
+        and is open sourced on{" "}
+        <a
+          href="
+          https://github.com/mayaclaros/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>{" "}
+        and hosted on
+        <a
+          href="https://react-weather-app-maya.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }
